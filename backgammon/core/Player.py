028260289f61240278321ae8f8c392c6ce2ae3aa
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2025-08-25 17:48:04 -0300

    Agrego la clase player para representar a un jugador

---
+++ backgammon/core/Player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, nombre, color):
+        self.nombre = nombre
+        self.color = color
+
+    def get_name(self):
+        return self.nombre
+    
+    def get_color(self):
+        return self.color
+    
