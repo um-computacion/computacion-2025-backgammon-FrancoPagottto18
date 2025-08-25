2e9f54807069b7bbd1d6d70508b3cb1324695884
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2025-08-25 17:40:24 -0300

    Correccion de error

---
+++ backgammon/core/checker.py
@@ -0,0 +1,16 @@
+class Checker:
+    """
+    Representa una ficha de Backgammon
+    """
+    def __init__(self, simbolo):
+        """
+        Inicializa una ficha con el color del jugador
+        """
+        self.__color__ = color
+
+    def get_color(self):
+        """Devuelve el color de la ficha"""
+        return self.__color__
+
+    def __str__(self):
+        return self.__color__
