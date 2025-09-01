2184b41923bfcf40c3752b1d7650399fe42cc0fc
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2025-09-01 18:38:04 -0300

    Agrego la funcion tirar

--- backgammon/core/dice.py
+++ backgammon/core/dice.py
@@ -3,8 +3,17 @@ import random
 class Dice:
     def __init__(self):
         """
-        Inicializa dos dados de seis caras.
+        Inicializa dos dados de seis caras
         """
         self.__dado1__ = 0
         self.__dado2__ = 0
         self.__tirada_doble__ = False
+    def tirar(self):
+        """
+        Tira los dos dados y retorna los valores
+        Si es doble, los valores se repiten
+        """
+        self.__dado1__ = random.randint(1, 6)
+        self.__dado2__ = random.randint(1, 6)
+        self.__tirada_doble__ = (self.__dado1__ == self.__dado2__)
+        return self.get_valores()
