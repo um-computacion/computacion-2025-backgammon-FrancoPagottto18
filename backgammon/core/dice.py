8164ea1ebe6fdc8e55920ece1cac2c18d8c3c4e2
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2025-08-31 22:39:53 -0300

    comienzo la implementacion del dado del backgammon

---
+++ backgammon/core/dice.py
@@ -0,0 +1,10 @@
+import random
+
+class Dice:
+    def __init__(self):
+        """
+        Inicializa dos dados de seis caras.
+        """
+        self.__dado1__ = 0
+        self.__dado2__ = 0
+        self.__tirada_doble__ = False
