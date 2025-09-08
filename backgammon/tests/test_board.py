6d09faac98578c5086eeadce4e0aa06e2e4f5c2f
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2025-09-08 14:23:53 -0300

    Agrego tests para agregar y quitar fichas del punto

--- backgammon/tests/test_board.py
+++ backgammon/tests/test_board.py
@@ -14,5 +14,17 @@ class TestBoard(unittest.TestCase):
         self.assertEqual(len(puntos[11]), 5)
         self.assertEqual(len(puntos[16]), 3)
         self.assertEqual(len(puntos[18]), 5)
+    def test_agregar_ficha_en_punto(self):
+        b = Board()
+        b.agregar_ficha("blanco", 1)
+        self.assertEqual(len(b.get_puntos()[1]), 1)
+
+    def test_quitar_ficha_en_punto(self):
+        b = Board()
+        b.agregar_ficha("blanco", 1)
+        ok = b.quitar_ficha(1)
+        self.assertTrue(ok)
+        self.assertEqual(len(b.get_puntos()[1]), 0)
+        self.assertFalse(b.quitar_ficha(1))
 
     
