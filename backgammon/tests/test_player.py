12a4eecee047641fb1a4098c3062b2c95ed6e9e4
Franco Pagotto <f.pagotto@alumno.um.edu.ar>
2025-09-18 00:27:49 -0300

    Agrego tests de get_name y get_color

--- backgammon/tests/test_player.py
+++ backgammon/tests/test_player.py
@@ -6,4 +6,11 @@ class TestPlayer(unittest.TestCase):
         player = Player("Juan", "blanco")
         self.assertEqual(player.get_name(), "Juan")
         self.assertEqual(player.get_color(), "blanco")
+    def test_get_name_(self):
+        player = Player("María", "blanco")
+        self.assertEqual(player.get_name(), "María")
+
+    def test_get_color_(self):
+        player = Player("Pedro", "negro")
+        self.assertEqual(player.get_color(), "negro")
 
